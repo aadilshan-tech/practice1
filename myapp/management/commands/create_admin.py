10a02e1d47626b4c1bfec9d1f81971fd46bb576e
Aadil Shan <aadilshan5065@gmail.com>
2026-01-05 21:22:53 +0530

    Remove temporary superuser creation logic

--- myapp/management/commands/create_admin.py
+++ myapp/management/commands/create_admin.py
@@ -1,20 +1,20 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+# from django.core.management.base import BaseCommand
+# from django.contrib.auth.models import User
 
-class Command(BaseCommand):
-    help = "Create default superuser"
+# class Command(BaseCommand):
+#     help = "Create default superuser"
 
-    def handle(self, *args, **kwargs):
-        username = "aadilshan"
-        email = "aadilshan5065@gmail.com"
-        password = "Rasheed@5065"
+#     def handle(self, *args, **kwargs):
+#         username = "aadilshan"
+#         email = "aadilshan5065@gmail.com"
+#         password = "Rasheed@5065"
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
-            self.stdout.write("Superuser created")
-        else:
-            self.stdout.write("Superuser already exists")
+#         if not User.objects.filter(username=username).exists():
+#             User.objects.create_superuser(
+#                 username=username,
+#                 email=email,
+#                 password=password
+#             )
+#             self.stdout.write("Superuser created")
+#         else:
+#             self.stdout.write("Superuser already exists")
